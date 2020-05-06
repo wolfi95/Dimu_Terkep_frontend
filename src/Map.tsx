67d90159e2a6a3642ec887ec9b417538088d64e6
danielkrausz <dnielkrausz@gmail.com>
2020-05-06 12:14:16 +0200

    refactor instfilter into function comp, pass checkbox values to map comp

--- src/Map.tsx
+++ src/Map.tsx
@@ -21,125 +21,136 @@ export interface IIntezmeny {
   link: string;
 }
 
-function Map() {
+const Map = () => {
+  const checkedValues = [0,1,2,3,4,5,6,7,8,9,10,11]
+  const [instTypes, setActiveInstTypes] = useState(checkedValues)
   const [activeInstitute, setActiveInistitute] = useState<
     IIntezmeny | undefined
   >(undefined);
   const [pins, setPins] = useState([]);
+  const onFilterChange = (data) => {
+    const toggleArrayValue =
+    instTypes.includes(data)
+    ? instTypes.filter(el => el !== data)
+    : [...instTypes, data]
+    setActiveInstTypes(toggleArrayValue)
+    console.log(data)
+  }
   React.useEffect(() => {
     const fetchData = async () => {
       const result = await axios.post("/Intezmeny", {
         mukodestol: 0,
+        intezmenyTipus: instTypes
       });
-
+      
       setPins(result.data);
     };
 
     fetchData();
-  }, []);
+  }, [instTypes]);
 
-  useWhatChanged([pins, activeInstitute]);
+  useWhatChanged([pins, activeInstitute, instTypes]);
 
   return (
     <React.Fragment>
-    <InstituteSearch/>
-    <InstFilter/>
-    <LeafletMap
-      center={[47.4979, 19.0402]}
-      zoom={14}
-      maxZoom={50}
-      attributionControl={true}
-      zoomControl={true}
-      doubleClickZoom={true}
-      scrollWheelZoom={true}
-      dragging={true}
-      animate={true}
-      easeLinearity={0.35}
-    >
-      <TileLayer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png" />
+      <InstituteSearch />
+      <InstFilter instTypes = {instTypes} onFilterChange = {(data) => { onFilterChange(data) }} />
+      <LeafletMap
+        center={[47.4979, 19.0402]}
+        zoom={14}
+        maxZoom={50}
+        attributionControl={true}
+        zoomControl={true}
+        doubleClickZoom={true}
+        scrollWheelZoom={true}
+        dragging={true}
+        animate={true}
+        easeLinearity={0.35}
+      >
+        <TileLayer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png" />
 
-      {pins.map((institute, index) => (
-        <Marker
-          key={index}
-          position={[institute["latitude"], institute["longitude"]]}
-          onClick={async () => {
-            try {
-              let result = await axios.get(
-                "Intezmeny/" + institute["intezmenyId"]
-              );
-              console.log(result);
-              setActiveInistitute(result.data);
-            } catch (e) {
-              console.log(e);
-            }
-          }}
-        />
-      ))}
+        {pins.map((institute, index) => (
+          <Marker
+            key={index}
+            position={[institute["latitude"], institute["longitude"]]}
+            onClick={async () => {
+              try {
+                let result = await axios.get(
+                  "Intezmeny/" + institute["intezmenyId"]
+                );
+                console.log(result);
+                setActiveInistitute(result.data);
+              } catch (e) {
+                console.log(e);
+              }
+            }}
+          />
+        ))}
 
-      {activeInstitute && (
-        <Popup
-          position={[
-            activeInstitute["intezmenyHelyszinek"][0].latitude,
-            activeInstitute["intezmenyHelyszinek"][0].longitude,
-          ]}
-          onClose={() => {
-            setActiveInistitute(undefined);
-          }}
-        >
-          <div>
-            <Grid
-              className="link-block"
-              container
-              direction="row"
-              alignItems="center"
-              style={{ flexWrap: "nowrap" }}
-            >
-              <Grid item>
-                <div className="inst-type-icon">
-                  {renderInstituteIcon(activeInstitute.tipus)}
-                </div>
+        {activeInstitute && (
+          <Popup
+            position={[
+              activeInstitute["intezmenyHelyszinek"][0].latitude,
+              activeInstitute["intezmenyHelyszinek"][0].longitude,
+            ]}
+            onClose={() => {
+              setActiveInistitute(undefined);
+            }}
+          >
+            <div>
+              <Grid
+                className="link-block"
+                container
+                direction="row"
+                alignItems="center"
+                style={{ flexWrap: "nowrap" }}
+              >
+                <Grid item>
+                  <div className="inst-type-icon">
+                    {renderInstituteIcon(activeInstitute.tipus)}
+                  </div>
+                </Grid>
+                <Grid item>
+                  <h2 style={{ marginBottom: "0px", marginTop: "0px" }}>
+                    {activeInstitute.nev}
+                  </h2>
+                  <div
+                    className="subtitle"
+                    style={{ marginBottom: "0px", marginTop: "0px" }}
+                  >
+                    ({activeInstitute.alapitas} - {activeInstitute.megszunes})
+                  </div>
+                </Grid>
               </Grid>
-              <Grid item>
-                <h2 style={{ marginBottom: "0px", marginTop: "0px" }}>
-                  {activeInstitute.nev}
-                </h2>
-                <div
-                  className="subtitle"
-                  style={{ marginBottom: "0px", marginTop: "0px" }}
-                >
-                  ({activeInstitute.alapitas} - {activeInstitute.megszunes})
-                </div>
-              </Grid>
-            </Grid>
 
-            <Grid
-              className="link-block"
-              container
-              direction="row"
-              alignItems="center"
-            >
-              <Grid item>
-                <LinkIcon className="link-icon" />
+              <Grid
+                className="link-block"
+                container
+                direction="row"
+                alignItems="center"
+              >
+                <Grid item>
+                  <LinkIcon className="link-icon" />
+                </Grid>
+                <Grid item>
+                  <Link
+                    href={activeInstitute.link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {activeInstitute.link !== "" ? activeInstitute.link : "-"}
+                  </Link>
+                </Grid>
               </Grid>
-              <Grid item>
-                <Link
-                  href={activeInstitute.link}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {activeInstitute.link !== "" ? activeInstitute.link : "-"}
-                </Link>
-              </Grid>
-            </Grid>
-            <div className="instDesc">
-              {activeInstitute.leiras !== "" ? activeInstitute.leiras : "-"}
+              <div className="instDesc">
+                {activeInstitute.leiras !== "" ? activeInstitute.leiras : "-"}
+              </div>
             </div>
-          </div>
-        </Popup>
-      )}
-    </LeafletMap>
+          </Popup>
+        )}
+      </LeafletMap>
     </React.Fragment>
-    );
-}
+  );
+};
 
 export default Map;
--- src/components/InstTypeFilters.tsx
+++ src/components/InstTypeFilters.tsx
@@ -1,22 +1,13 @@
 import React from "react";
 import { Checkbox, FormControlLabel } from "@material-ui/core";
 
-class InstFilter extends React.Component<{}, { checkedValues: Array<number> }> {
-    constructor(props) {
-    super(props);
-    this.handleFilter = this.handleFilter.bind(this);
-    this.state = {
-      checkedValues: [0,1,2,3,4,5,6,7,8,9,10,11],
-    };
-  }
-  handleFilter(instituteType) {
-    this.setState((state) => ({
-        checkedValues: state.checkedValues.includes(instituteType) ? state.checkedValues.filter(i => i !== instituteType) : [ ...state.checkedValues, instituteType ]
-    }));
-    console.log(this.state.checkedValues);
-  }
+const InstFilter = (props) => {
 
-  labelList = [
+    const handleChange = (id) => {
+        props.onFilterChange(id)    
+    }
+
+  const labelList = [
     { label: "Állami Múzeum", id: 0 },
     { label: "Állami Kulturális Intézet", id: 1 },
     { label: "Önkormányzati Múzeum", id: 2 },
@@ -31,20 +22,18 @@ class InstFilter extends React.Component<{}, { checkedValues: Array<number> }> {
     { label: "Étterem Kocsma Galéria", id: 11 },
   ];
 
-  render() {
     return (
       <div className="filterBlock">
-        {this.labelList.map((element) => (
+        {labelList.map((element) => (
           <FormControlLabel
             control={<Checkbox key={element.id} />}
             label={element.label}
-            onChange={this.handleFilter.bind(this, element.id)}
-            checked={this.state.checkedValues.includes(element.id)}
+            onChange={() => handleChange(element.id)}
+            checked={props.instTypes.includes(element.id)}
           />
         ))}
       </div>
     );
-  }
 }
 
 export default InstFilter;
