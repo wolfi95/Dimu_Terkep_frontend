ebefd75f266c70c132bc2e96f97f3947a8c18893
danielkrausz <dnielkrausz@gmail.com>
2020-05-10 23:37:38 +0200

    Add multiple search categories

--- src/components/InstituteSearch.tsx
+++ src/components/InstituteSearch.tsx
@@ -1,17 +1,65 @@
-import React from "react";
-import { TextField } from "@material-ui/core";
+import React, { useState } from "react";
+import {
+  TextField,
+  FormControl,
+  InputLabel,
+  MenuItem,
+} from "@material-ui/core";
+import { Select } from "@material-ui/core";
+import { SearchTypeId, SearchTypeName } from "../enums/enums";
 
 const InstituteSearch = (props) => {
+  const [searchFieldLabel, setSearchFieldLabel] = useState(
+    SearchTypeName.IntezmenyNev as string
+  );
+  const [searchFieldText, setSearchFieldText] = useState("");
+  let menuItems: string[] = [];
+  for (const key in SearchTypeName) {
+    menuItems.push(SearchTypeName[key]);
+  }
   const handleChange = (event) => {
+    setSearchFieldText(event.target.value);
     props.onSearchValChange(event.target.value);
+  };
+  const handleSelectChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    setSearchFieldLabel(event.target.value as string);
+    setSearchFieldText("");
+    let searchTypeTerm = "";
+    switch (event.target.value) {
+      case SearchTypeName.IntezmenyNev:
+        searchTypeTerm = SearchTypeId.IntezmenyNev;
+        break;
+      case SearchTypeName.IntezmenyCim:
+        searchTypeTerm = SearchTypeId.IntezmenyCim;
+        break;
+      case SearchTypeName.IntezmenyVezeto:
+        searchTypeTerm = SearchTypeId.IntezmenyVezeto;
+        break;
+    }
+    props.onSearchTypeChange(searchTypeTerm as string);
   };
 
   return (
     <div className="searchBlock">
+      <FormControl variant="filled" className="searchSelectForm">
+        <InputLabel id="searchSelectLabel">Keresés</InputLabel>
+        <Select
+          className="searchTypeSelector"
+          labelId="searchSelectLabelId"
+          id="SearchSelect"
+          onChange={handleSelectChange}
+          value={searchFieldLabel}
+        >
+          {menuItems.map((value, index) => (
+            <MenuItem value={value}>{value}</MenuItem>
+          ))}
+        </Select>
+      </FormControl>
       <TextField
         id="instSearch"
         type="search"
-        label="Intézmény keresés"
+        value={searchFieldText}
+        label={searchFieldLabel}
         variant="filled"
         onChange={(event) => handleChange(event)}
       />
