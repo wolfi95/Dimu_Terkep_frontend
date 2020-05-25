2cd1f637d6834b8f940c73ec0f8fb9593119ca9e
danielkrausz <dnielkrausz@gmail.com>
2020-05-25 18:51:29 +0200

    Add new routes to login & admin pages with react-router

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Map from './Map';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import Map from "./Map";
+import { Route, BrowserRouter as Router, Redirect, Switch } from "react-router-dom";
+import * as serviceWorker from "./serviceWorker";
+import Admin from "./Admin";
+import SignIn from "./Login";
 
-ReactDOM.render(<Map />, document.getElementById('root'));
+
+const routing = (
+    <Router>
+        <Switch>
+            <Route exact path="/" component={Map} />
+            <Route exact path="/admin" component={Admin} />
+            <Route exact path="/login" component={SignIn} />
+            <Route render={() => <Redirect to={{pathname: "/"}} />} />
+        </Switch>
+    </Router>
+);
+
+ReactDOM.render(routing, document.getElementById("root"));
+
+// const loggedIn = () => {
+//     // ...
+//   }
+  
+
+// function requireAuth(nextState, replace) {
+//     if (!loggedIn()) {
+//       replace({
+//         pathname: '/login'
+//       })
+//     }
+//   }
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
