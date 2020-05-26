381cd13d79137d0eba0f572c3b27f3d3de587efe
wolfi95 <szalkaititi@gmail.com>
2020-05-26 16:17:39 +0200

    url fix

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://localhost:44376/api/",
+  baseURL: "https://dimu-backend.herokuapp.com/api/",
   timeout: 5000,
   headers: {
     "Content-Type": "application/json"
